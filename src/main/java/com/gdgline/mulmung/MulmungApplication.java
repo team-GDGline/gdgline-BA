ad7c5d2a26f5a663bdc075c552ef8be6e07b295e
LeeDongjae <leedongjae625@gmail.com>
2024-11-13 19:45:43 +0900

    Env: spring boot 프로젝트 생성

---
+++ src/main/java/com/gdgline/mulmung/MulmungApplication.java
@@ -0,0 +1,13 @@
+package com.gdgline.mulmung;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MulmungApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MulmungApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gdgline/mulmung/MulmungApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gdgline.mulmung;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MulmungApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
